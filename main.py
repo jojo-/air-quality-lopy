a57402568bdd8675e9a214458095cec72fdc5362
Nicolas Verstaevel <n.verstaevel@gmail.com>
2018-11-07 09:06:52 +1100

    Add files via upload
    Add try/except statement

--- main.py
+++ main.py
@@ -12,33 +12,37 @@ from EasyLoraConnect import EasyLoraConnect
 import gc
 import time
 import struct
+import machine
 
-lora = EasyLoraConnect()
-#Init the SEN0177 Sensor
-sds011 = SDS011(PTX='P11',PRX='P10')
+try:
+    lora = EasyLoraConnect()
+    #Init the SEN0177 Sensor
+    sds011 = SDS011(PTX='P11',PRX='P10')
 
-while True:
-        #Read sensor data
-        error = False
-        (pm2_5,pm10) = sds011._read_PM()
-        if not sds011._isError():
-            print('===============================')
-            #print('pm1  =' + str(pm1) + ' ug/m3')
-            print('pm2.5 =' + str(pm2_5) + ' ug/m3')
-            print('pm10  =' + str(pm10) + ' ug/m3')
-            #lora._send_LPP_over_lora([str(pm2_5),str(pm10)])
-        else:
-            print('Oups...something went wrong in the reading')
-            error = True
+    while True:
+            #Read sensor data
+            error = False
+            (pm2_5,pm10) = sds011._read_PM()
+            if not sds011._isError():
+                print('===============================')
+                #print('pm1  =' + str(pm1) + ' ug/m3')
+                print('pm2.5 =' + str(pm2_5) + ' ug/m3')
+                print('pm10  =' + str(pm10) + ' ug/m3')
+                #lora._send_LPP_over_lora([str(pm2_5),str(pm10)])
+            else:
+                print('Oups...something went wrong in the reading')
+                error = True
 
-        ttn_data = bytes()
-        ttn_data = ttn_data + struct.pack('!l', pm2_5)
-        ttn_data = ttn_data + struct.pack('!l', pm10)
-        checksum = pm2_5+pm10
-        ttn_data = ttn_data + struct.pack('!l', checksum)
-        print("Sending : "+str(ttn_data))
-        lora.send(ttn_data)
-        
-        gc.collect()
-        pycom.rgbled(0x4B0082)
-        time.sleep(config.INT_SAMPLING)
+            ttn_data = bytes()
+            ttn_data = ttn_data + struct.pack('!l', pm2_5)
+            ttn_data = ttn_data + struct.pack('!l', pm10)
+            checksum = pm2_5+pm10
+            ttn_data = ttn_data + struct.pack('!l', checksum)
+            print("Sending : "+str(ttn_data))
+            lora.send(ttn_data)
+            
+            gc.collect()
+            pycom.rgbled(0x4B0082)
+            time.sleep(config.INT_SAMPLING)
+except:
+    machine.reset()
